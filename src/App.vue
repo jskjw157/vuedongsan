967f7edd13e10b46638b93b3cbd2516b974e5e38
johny <jskjw157@gmail.com>
2022-11-15 16:41:20 +0900

    [MODIFY][메인페이지] 기본 문법 EXAMPLE

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  init
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    원룸샵
+    <h4 :style="스타일"> {{ products[0] }}</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      price1 : 60,
+      price2 : 70,
+      스타일 : 'color : blue',
+      products : ['역삼동원룸','천호동원룸','마포구원룸'],
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
